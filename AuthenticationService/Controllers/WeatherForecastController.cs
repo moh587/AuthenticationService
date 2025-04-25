943fd1a44d25242d49c121a32c3def02719801a7
moh587 <moh.adel587@gmail.com>
2025-04-25 16:46:34 +0300

    Rollback

--- AuthenticationService/Controllers/WeatherForecastController.cs
+++ AuthenticationService/Controllers/WeatherForecastController.cs
@@ -18,7 +18,7 @@ namespace AuthenticationService.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecastt")]
+        [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
